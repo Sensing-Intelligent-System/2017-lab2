857e6606216ba40b37ccf5f14083283fd2aab41e
kuokuo <c722996@gmail.com>
2017-09-24 23:08:00 +0800

    modify environment path

--- environment.sh
+++ environment.sh
@@ -17,7 +17,7 @@ echo "...done"
 echo "Activating development."
 source $DUCKIETOWN_ROOT/catkin_ws/devel/setup.bash
 
-source lab2/catkin_ws/devel/setup.bash
+source ~/lab2/catkin_ws/devel/setup.bash
 
 # TODO: check that the time is >= 2015
 
